4fa6cf3c25f5aaf8cbf1436c18b5aba95047c085
Emilia <90127060+EmiliaPrzybylek@users.noreply.github.com>
2022-01-23 17:22:31 +0100

    Delete Bootstrap_project_1 directory

--- Bootstrap_project_1/js/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-  const nav = document.querySelector('.navbar-collapse')
-
-  document.addEventListener('click', () => {
-    if (nav.classList.contains('show')) {
-      nav.classList.remove('show')
-    }
-  })
